d9ae9fa37628abc31680ce83e72932f9a135ef4f
skydebaun <sdeba001@ucr.edu>
2020-04-17 01:17:42 -0700

    MQTT publication for shadow working

---
+++ skywalker/shadow_test01.py
@@ -0,0 +1,85 @@
+'''
+/*
+ * 
+ *
+ * 
+ */
+ '''
+
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
+#import logging
+import time
+import json
+#import argparse
+
+
+# Shadow JSON schema:
+#
+# Name: Bot
+# {
+#	"state": {
+#		"desired":{
+#			"property":<INT VALUE>
+#		}
+#	}
+# }
+
+# Custom Shadow callback
+def customShadowCallback_Update(payload, responseStatus, token):
+    # payload is a JSON string ready to be parsed using json.loads(...)
+    # in both Py2.x and Py3.x
+    if responseStatus == "timeout":
+        print("Update request " + token + " time out!")
+    if responseStatus == "accepted":
+        payloadDict = json.loads(payload)
+        print("~~~~~~~~~~~~~~~~~~~~~~~")
+        print("Update request with token: " + token + " accepted!")
+        print("property: " + str(payloadDict["state"]["desired"]["property"]))
+        print("~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+    if responseStatus == "rejected":
+        print("Update request " + token + " rejected!")
+
+def customShadowCallback_Delete(payload, responseStatus, token):
+    if responseStatus == "timeout":
+        print("Delete request " + token + " time out!")
+    if responseStatus == "accepted":
+        print("~~~~~~~~~~~~~~~~~~~~~~~")
+        print("Delete request with token: " + token + " accepted!")
+        print("~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+    if responseStatus == "rejected":
+        print("Delete request " + token + " rejected!")
+
+
+
+
+# Init AWSIoTMQTTShadowClient
+myAWSIoTMQTTShadowClient = None
+
+#skywalkers shadow
+myAWSIoTMQTTShadowClient = AWSIoTMQTTShadowClient("333052c1bf")#this can be any arbitrary string
+myAWSIoTMQTTShadowClient.configureEndpoint("a3te7fgu4kv468-ats.iot.us-west-1.amazonaws.com", 8883)#endpoint and port number
+myAWSIoTMQTTShadowClient.configureCredentials("cert/rootCA.pem.crt", "cert/333052c1bf-private.pem.key", "cert/333052c1bf-certificate.pem.crt")#root ca and certificate used for secure connection
+
+
+
+# AWSIoTMQTTShadowClient configuration
+myAWSIoTMQTTShadowClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTShadowClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTShadowClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect to AWS IoT
+myAWSIoTMQTTShadowClient.connect()
+
+# Create a deviceShadow with persistent subscription
+deviceShadowHandler = myAWSIoTMQTTShadowClient.createShadowHandlerWithName("Pi_sense01", True)
+
+# Delete shadow JSON doc
+deviceShadowHandler.shadowDelete(customShadowCallback_Delete, 5)
+
+# Update shadow in a loop
+loopCount = 0
+while True:
+    JSONPayload = '{"state":{"desired":{"property":' + str(loopCount) + '}}}'
+    deviceShadowHandler.shadowUpdate(JSONPayload, customShadowCallback_Update, 5)
+    loopCount += 1
+    time.sleep(1)
