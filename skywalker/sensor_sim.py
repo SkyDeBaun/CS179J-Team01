8c9b719fa3f82d0199aa055bf12eeb163924fb3c
skydebaun <sdeba001@ucr.edu>
2020-04-20 01:55:56 -0700

    clean up code - publishes only

--- skywalker/sensor_sim.py
+++ skywalker/sensor_sim.py
@@ -5,7 +5,7 @@
 #includes--------------------------------------------------------------------
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 from time import sleep
-from datetime import date, datetime 
+from datetime import date, datetime
  
 # AWS IoT certificate based connection---------------------------------------
 myMQTTClient = AWSIoTMQTTClient("333052c1bf")#this can be any arbitrary string
@@ -21,13 +21,6 @@ myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 myMQTTClient.connect()
 myMQTTClient.publish("Pi_sense01/info", "connected", 0) # #Pi_sense01 is the name of Thing on AWS IoT
  
-# Create a device shadow handler, use this to update and delete shadow document
-#deviceShadowHandler = myMQTTClient.createShadowHandlerWithName(Pi_sense01, True)
-
-# Delete current shadow JSON doc
-#deviceShadowHandler.shadowDelete(customShadowCallback_Delete, 5)
-
-
 
 #----------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
@@ -36,13 +29,12 @@ while 1:
     now = datetime.utcnow()#iso timestamp
     now_str = now.strftime('%Y-%m-%dT%H:%M:%SZ') #e.g. 2016-04-18T06:12:25.877Z
     result = 777 #read from sensor value instead
+    
     if result > 500: #if result = True
         payload = '{ "timestamp": "' + now_str + '","temperature": ' + str(result) + ',"humidity": '+ str(99.99) + ' }'
         print (payload) #print message to console
-        #myMQTTClient.publish("Pi_sense01/data", payload, 0) #Pi_sense01 is the name of Thing on AWS IoT
         myMQTTClient.publish("Pi_sense01/data", payload, 0)
-        #deviceShadowHandler.shadowUpdate(json.dumps(payload), customShadowCallback_Update, 5)
-        sleep(5)
+        sleep(10)
     else:
         print (".")
         sleep(1)
