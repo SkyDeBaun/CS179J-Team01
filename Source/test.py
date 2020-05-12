b6114c3bf09dbb6f686bc760d1fc9e77d1ceaef4
UyTran <uytran9321@gmail.com>
2020-05-12 10:47:24 -0700

    Wrong test file

--- Source/test.py
+++ Source/test.py
@@ -1,27 +1,21 @@
 
 import subscriptionFunctions
 import shadowFunctions
-import pytest
+import pytest #My local machine doesn't like this
+import inspect
 
 # content of test_sample.py
-def func(x):
-    return x + 1
+# def func(x):
+#     return x + 1
 
+# @pytest.mark.parametrize("input, expected", [(4, 5), (3, 6), (41, 42), (3, 6)])
+# def test_functions(input, expected):
+#   assert(func(input) == expected)
 
-def test_answer():
-    assert func(2) == 3
-    assert func(3) == 5
-    assert func(2) == 3
-    assert func(6) == 5
-
-
-@pytest.mark.parametrize("input, expected", [(4, 5), (3, 6), (41, 42), (3, 6)])
-def test_functions(input, expected):
-  assert(func(input) == expected)
-
-
-# functionList = list(subscribedTopicDictionary.values())
+functionList = list(subscriptionFunctions.subscribedTopicDictionary.values())
+functionList.append(print)
 # for funct in functionList:
-# @pytest.mark.parametrize("function, message, expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
-# def test_functions(function, message):
+@pytest.mark.parametrize("function", functionList)
+def test_functions(function, message):
+  assert len(inspect.signature(function).parameters) == 3, "Actual amount of parameters {}".format(len(inspect.signature(function).parameters))
 
