895e98d6404738ca536ac1c502bdfcb1ccf069f9
UyTran <uytran9321@gmail.com>
2020-05-12 12:05:46 -0700

    ./gitAutoCommit.sh

--- gitAutoCommit.sh
+++ gitAutoCommit.sh
@@ -3,9 +3,10 @@ message=${0}
 
 git status
 git add Source/*;
+git add *.sh;
 git commit -m "$message";
-branchName = $(git rev-parse --abbrev-ref HEAD);
+branchName=$(git rev-parse --abbrev-ref HEAD);
 
-git push origin $(branchName)
+git push origin $branchName
 
 
