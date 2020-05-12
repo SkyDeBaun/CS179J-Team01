83409c7b7661c3f60ae24d7e57570dab55266683
UyTran <uytran9321@gmail.com>
2020-05-12 10:21:42 -0700

    Function test fixtures

--- Source/test.py
+++ Source/test.py
@@ -1,6 +1,7 @@
 
 import subscriptionFunctions
 import shadowFunctions
+import pytest
 
 # content of test_sample.py
 def func(x):
@@ -11,4 +12,14 @@ def test_answer():
     assert func(2) == 3
     assert func(3) == 5
     assert func(2) == 3
+
+@pytest.mark.parametrize("input, expected", [(4, 5), (3, 6), (41, 42)])
+def test_functions(input, expected):
+  assert(func(input) == expected)
+
+
+# functionList = list(subscribedTopicDictionary.values())
+# for funct in functionList:
+# @pytest.mark.parametrize("function, message, expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
+# def test_functions(function, message):
 
