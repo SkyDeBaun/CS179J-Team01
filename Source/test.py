43c2eb3b5f7c9c1548eb2becabae5be8f9bf2f86
UyTran <uytran9321@gmail.com>
2020-05-20 10:33:55 -0700

    Updated test.py template and example

--- Source/test.py
+++ Source/test.py
@@ -15,14 +15,18 @@ import json
 
 
 #################################################################################
-# Example parametrized test function 
+# Template parametrized test function 
 # All capitalized letter words are where things need to be filled in
-# @pytest.mark.parametrize("INPUT, EXPECTED, ANY_ADDITIONAL_PARAMETERS_THAT_MAY_CHANGE", [(FIRST_INPUT_VALUE, FIRST_EXPECTED_VALUE), (SECOND_INPUT_VALUE, SECOND_EXPECTED_VALUE)])
 # The name of test should have "test_" affixiated to the front
+# INPUT and EXPECTED are automatically filled in in the function call with the items from the list
+# @pytest.mark.parametrize("INPUT, EXPECTED, ANY_ADDITIONAL_PARAMETERS_THAT_MAY_CHANGE", [(FIRST_INPUT_VALUE, FIRST_EXPECTED_VALUE), (SECOND_INPUT_VALUE, SECOND_EXPECTED_VALUE)])
 # def NAME_OF_TEST_FUNCTION(INPUT, EXPECTED):
 #   assert(CALLBACK_FUNCTION(None, None, INPUT) == EXPECTED)
 #
-#
+# Example if value is greater than 15
+# @pytest.mark.parametrize("data, expectedStatus", [(25, True), (10, False), (1000, True), (1, False), (15, False)])
+# def test_greaterThan15(INPUT, EXPECTED):
+#   assert(greaterThan15(None, None, data) == expectedStatus)
 #
 #################################################################################
 
