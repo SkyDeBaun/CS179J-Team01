6dc32d912084f9c6a7cd5f804257b0352de9ba16
UyTran <uytran9321@gmail.com>
2020-05-21 14:08:57 -0700

    Temporarily commenting out Reyna code to test Travis

--- Source/subscriptionFunctions.py
+++ Source/subscriptionFunctions.py
@@ -1,5 +1,5 @@
 import cameraCode
-import reynaPiNode
+#import reynaPiNode
 import helpers
 import RPi.GPIO as GPIO
 import json
@@ -64,8 +64,8 @@ def motor2(client, userdate, message):
 subscribedTopicDictionary = {
   "picture" : picture,
   "controlFan" : controlFan,
-  "ultrasonic" : ultrasonic,
-  "motor2" : motor2
+ # "ultrasonic" : ultrasonic,
+ # "motor2" : motor2
   #FIXME Find some way to not hardcode value names
 }
 
