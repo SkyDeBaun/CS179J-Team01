fbddbe522d0e94469e08a5403908f88c0971030d
UyTran <uytran9321@gmail.com>
2020-05-26 02:00:47 -0700

    Added imports to squelch the GPIO import

--- Source/testTravisCerts.py
+++ Source/testTravisCerts.py
@@ -1,3 +1,10 @@
+import sys
+import fake_rpi
+
+sys.modules['RPi'] = fake_rpi.RPi     # Fake RPi
+sys.modules['RPi.GPIO'] = fake_rpi.RPi.GPIO # Fake GPIO
+
+import pytest
 import functionalizedAWSIOT
 import helpers
 from time import sleep
