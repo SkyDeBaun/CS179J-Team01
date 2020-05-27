54c21ec670f7b3e73601177bb23f781086173f61
skydebaun <sdeba001@ucr.edu>
2020-05-27 14:58:22 -0700

    simple radio file added -> has init and global radio object + config details

---
+++ Source/functionalizedRadio.py
@@ -0,0 +1,17 @@
+
+# rfm radio---------------------------------------------------
+from RFM69 import Radio, FREQ_915MHZ
+
+# radio tranceiver configuration-------------------------------
+# -------------------------------------------------------------
+node_id = 1  # hub node (this)
+network_id = 100  # 1 - 255
+key = "sampleEncryptKey"  # must be shared accross all radios on the radio net
+
+#expected globals-----------------------------------------------
+radio = None #make radio available as global object
+
+
+def initializeRadio():
+    radio = Radio(FREQ_915MHZ, node_id, network_id, encryptionKey=key, isHighPower=True, verbose=False)
+    return radio
