346e35e08787746616dd6ec66a93ab66d0c7b00f
skydebaun <sdeba001@ucr.edu>
2020-05-28 22:58:30 -0700

    test success -> no errors reported

--- Source/test.py
+++ Source/test.py
@@ -85,20 +85,3 @@ def test_fanOperational(message, expectedStatus):
   assert subscriptionFunctions.subscribedTopicDictionary["controlFan"](None, None, message) == expectedStatus
 
 
-
-#Test data
-data5 = '{ "Temperature": ' + "20" + ',"Light": '+ "50" + ' }'
-data6 = '{ "Temperature": ' + "40" + ',"Light": '+ "100" + ' }'
-data7 = '{ "Temperature": ' + "15" + ',"Light": '+ "86" + ' }'
-data8 = '{ "Temperature": ' + "99" + ',"Light": '+ "84" + ' }'
-message1 = message(data5)
-message2 = message(data6)
-message3 = message(data7)
-message4 = message(data8)
-
-
-#test subHumiture-------------------------------?
-@pytest.mark.parametrize("message, expectedStatus", [(message1, 0), (message2, 1), (message3, 1), (message4, 0)])
-def test_fanOperational(message, expectedStatus):
-  assert subscriptionFunctions.subscribedTopicDictionary["subRadioNodes"](None, None, message) == expectedStatus
-
