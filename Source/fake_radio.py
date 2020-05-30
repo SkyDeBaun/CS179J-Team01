c18f9d0cc1930e4b12b956f7ce3e2e60770694e9
skydebaun <sdeba001@ucr.edu>
2020-05-29 23:43:23 -0700

    Working on faking radio
    have two issues:
    -unable to pass keyword arguments to fake_radio
    -generic test case in test.py prompts strange no attribute behaviour for fake radio object

--- Source/fake_radio.py
+++ Source/fake_radio.py
@@ -1,7 +1,18 @@
+import sys, time, logging
+from datetime import datetime
+import logging
+#import spidev
+#import RPi.GPIO as GPIO
+#from registers import *
+#from packet import Packet
+#from config import get_config
+
+
 class Radio():
 
     def __init__(self, freq, ID, networkID, **kwargs):
-        self.hello = 0
+        for key, value in kwargs.items(): 
+            print ("%s == %s" %(key, value)) 
 
     def _init_gpio(self):
         self.hello = 1
@@ -15,27 +26,35 @@ class Radio():
     def _set_config(self):
         self.hello = 3
 
-    def Radio(self):
-        self.hello = 3
-    def FREQ_915MHZ():
+    def Radio(self, var1, var2, var3, var4, var5):
         self.hello = 3
 
+    def FREQ_915MHZ(self):
+        self.hello = 3
 
     def _init_interrupt(self):
         self.hello = 3
+
     def __enter__(self):
         self.hello = 3
+
     def __exit__(self, *args):
         self.hello = 3
+
     def set_frequency(self, val):
         self.hello = 3
+
     def set_network(self, id):
         self.hello = 3
+
     def set_power_level(self):
         self.hello = 3
+
     def _send(self, to, buffer, val=False):
         self.hello = 3
+
     def broadcast(self):
         self.hello = 3
+
     def send(self, to, buff, **kwargs):
         self.hello = 3
--- Source/functionalizedRadio.py
+++ Source/functionalizedRadio.py
@@ -9,9 +9,11 @@ network_id = 100  # 1 - 255
 key = "sampleEncryptKey"  # must be shared accross all radios on the radio net
 
 #expected globals-----------------------------------------------
-radio = None #make radio available as global object
+radio = None #make radio available as global object?
 
 
 def initializeRadio():
+    #radio = Radio(FREQ_915MHZ, node_id, network_id, key, True, False)#pytest -> works, fails of production
     radio = Radio(FREQ_915MHZ, node_id, network_id, encryptionKey=key, isHighPower=True, verbose=False)
+
     return radio
