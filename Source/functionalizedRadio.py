b0bc9259523052bc2cf91bf53afe9db4edb16979
skydebaun <sdeba001@ucr.edu>
2020-05-31 14:23:33 -0700

    radio hub configured for pytest (only)

--- Source/functionalizedRadio.py
+++ Source/functionalizedRadio.py
@@ -10,7 +10,7 @@ key = "sampleEncryptKey"  # must be shared accross all radios on the radio net
 
 
 def initializeRadio():
-    #radio = Radio(FREQ_915MHZ, node_id, network_id, key, True, False)#pytest -> works, fails for production
-    radio = Radio(FREQ_915MHZ, node_id, network_id, encryptionKey=key, isHighPower=True, verbose=False)
+    radio = Radio(FREQ_915MHZ, node_id, network_id, key, True, False)# use for pytest -> fails for production
+    #radio = Radio(FREQ_915MHZ, node_id, network_id, encryptionKey=key, isHighPower=True, verbose=False) #use for production -> kwargs fails for pytest
 
     return radio
