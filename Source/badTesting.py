e7fd2a8dc934c47cfb257e0adc0967f2c91d5dfa
UyTran <uytran9321@gmail.com>
2020-05-30 18:57:07 -0700

    Removed unneeded files

--- Source/badTesting.py
+++
@@ -1,9 +0,0 @@
-import functionalizedAWSIOT
-import time 
-MQTTClient = functionalizedAWSIOT.AWS_MQTT_Initialize()
-
-functionalizedAWSIOT.AWS_MQTT_publish(MQTTClient, "testing", "testing message boop beep")
-
-
-while(1):
-  time.sleep(2)
